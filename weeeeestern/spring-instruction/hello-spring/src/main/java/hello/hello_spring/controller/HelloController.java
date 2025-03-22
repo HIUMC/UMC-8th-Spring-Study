889ef1938be5ad494c27db3fd1b08217e561a727
weeeeestern <liz0824@naver.com>
2025-03-22 17:22:36 +0900

    Feat(박은서): 02.프로젝트 환경설정

---
+++ weeeeestern/spring-instruction/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,16 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "Hello!!");
+        return "hello"; // resource의 templates의 "hello" 를 찾아서 렌더링 해라!!
+    }
+}
