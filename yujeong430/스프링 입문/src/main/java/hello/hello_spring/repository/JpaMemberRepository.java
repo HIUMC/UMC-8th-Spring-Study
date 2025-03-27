f22bcf4bf0147834159b940270d29b79fa8e1543
yujeong <parkyj0430@gmail.com>
2025-03-27 14:21:42 +0900

    스프링 입문

---
+++ yujeong430/스프링 입문/src/main/java/hello/hello_spring/repository/JpaMemberRepository.java
@@ -0,0 +1,43 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository{
+
+    // jpa를 쓰려면 entitymanager를 주입받아야 함
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+                    // 객체를 대상으로 쿼리를 날림
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+}
