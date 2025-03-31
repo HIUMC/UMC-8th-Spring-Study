88403b03e554e90052609e2105151c4d0a18ce0b
caminobelllo <alstpgml0812@gmail.com>
2025-03-31 15:47:56 +0900

    Fix: Merge conflict

--- caminobelllo/spring-basic/src/test/java/hello/spring_basic/order/OrderServiceTest.java
+++ caminobelllo/spring-basic/src/test/java/hello/spring_basic/order/OrderServiceTest.java
@@ -1,24 +1,16 @@
 package hello.spring_basic.order;
 
-import hello.spring_basic.AppConfig;
 import hello.spring_basic.member.Grade;
 import hello.spring_basic.member.Member;
 import hello.spring_basic.member.MemberService;
+import hello.spring_basic.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService;
-    OrderService orderService;
-
-    @BeforeEach
-    public void beforeEach() {
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
-        orderService = appConfig.orderService();
-    }
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
 
     @Test
     void createOrder() {
