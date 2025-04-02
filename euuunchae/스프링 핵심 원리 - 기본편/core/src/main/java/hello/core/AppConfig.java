13487c732cc674bf47de87d9a995546456bd99a2
GitHub 핸들명 <GitHub >
2025-04-03 03:00:08 +0900

    feat: 스프링으로 전환하기

--- euuunchae/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
+++ euuunchae/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy(); //새로운 할인 정책 적용
         //return new FixDiscountPolicy();
