e3fc7ba084170a295ae92570b9f518d210f8315f
이제원 <lys28750@naver.com>
2025-04-10 00:15:01 +0900

    week3

--- nowOne2/core/src/main/java/hello/core/CoreApplication.java
+++ nowOne2/core/src/main/java/hello/core/CoreApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoreApplication {
+	//3주차 정리 완료
 
 	public static void main(String[] args) {
 
