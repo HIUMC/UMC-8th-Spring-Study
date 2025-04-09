5f8d267178eeeaa3288aa1e7330b73a87dc75142
c5ln <cjstjdgh0209@gmail.com>
2025-04-09 21:55:10 +0900

    week3 core

--- c5ln/스프링 핵심 원리 - 기본/core/src/main/java/heelo/core/member/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package heelo.core.member;
-
-public interface MemberRepository {
-
-    void save(Member member);
-    Member findById(Long memberId);
-
-}
---
+++ c5ln/스프링 핵심 원리 - 기본/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,29 @@
+package hello.core.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired //ac.getBean 과 비슷한 동작
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+}
