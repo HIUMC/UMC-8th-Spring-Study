718a6c1e5793e4f4ac21d819c461828fdffd0bee
dldusgh318 <pololydotoly@gmail.com>
2025-04-28 12:05:28 +0900

    5주차 강의 완료

--- dldusgh318/core/src/main/java/hello/core/AutoAppConfig.java
+++ dldusgh318/core/src/main/java/hello/core/AutoAppConfig.java
@@ -4,11 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+
+@Configuration
+@ComponentScan(
+    excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
-
-    @Configuration
-    @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes =Configuration.class))
-    public class AutoAppConfig {
-
-    }
 }
