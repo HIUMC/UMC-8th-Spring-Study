1b586824cd40396dae75c23ce58cf8604006d0d3
GitHub 핸들명 <GitHub >
2025-05-01 05:58:14 +0900

    section 8

--- euuunchae/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AutoAppConfig.java
+++ euuunchae/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,7 +1,10 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -14,5 +17,10 @@ import org.springframework.context.annotation.FilterType;
 
 )
 public class AutoAppConfig {
-
+    /*
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memoryMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+     */
 }
