8ad33aa347884a2e068ac942cf463c2d6084cc9f
dldusgh318 <pololydotoly@gmail.com>
2025-05-03 16:24:36 +0900

    feat: 6주차 주니어 미션

--- dldusgh318/spring/src/main/java/umc/spring/domain/Store.java
+++ dldusgh318/spring/src/main/java/umc/spring/domain/Store.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 import umc.spring.domain.common.BaseEntity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Builder
@@ -25,4 +28,21 @@ public class Store extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "region_id")
     private Region region;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Mission> missionList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Store{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", rating=" + rating +
+                ", region=" + (region != null ? region.getName() : "N/A") + // region의 이름 출력
+                '}';
+    }
 }
--- dldusgh318/spring/src/main/java/umc/spring/repository/MemberRepository.java
+++ dldusgh318/spring/src/main/java/umc/spring/repository/MemberRepository.java
@@ -2,6 +2,11 @@ package umc.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.spring.domain.Member;
+import umc.spring.domain.enums.MemberStatus;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByNameAndMemberStatus(String name, MemberStatus status);
 }
