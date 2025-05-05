38b3888d6b0ab8952feeb39f1f085db92ad61ca9
finger9999 <161580182+finger9999@users.noreply.github.com>
2025-05-05 16:18:49 +0900

    6주차 완료

---
+++ finger9999/spring/src/main/java/umc/spring/repository/StoreRepository/StoreRepositoryCustom.java
@@ -0,0 +1,8 @@
+package umc.spring.repository.StoreRepository;
+
+import java.util.List;
+
+
+public interface StoreRepositoryCustom {
+    List<Object> dynamicQueryWithBooleanBuilder(String name, Float score);
+}
