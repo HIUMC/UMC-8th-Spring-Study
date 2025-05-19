80eb736d9cd56bbbe796d94134a04881a1e5a1a3
finger9999 <161580182+finger9999@users.noreply.github.com>
2025-05-19 15:51:52 +0900

    8주차 미션 완료:

--- finger9999/spring/week8/src/main/java/umc/spring/converter/StoreConverter.java
+++ finger9999/spring/week8/src/main/java/umc/spring/converter/StoreConverter.java
@@ -1,5 +1,6 @@
 package umc.spring.converter;
 
+<<<<<<< HEAD
 import umc.spring.domain.Region;
 import umc.spring.domain.Store;
 import umc.spring.web.dto.Store.StoreResponseDTO;
@@ -13,13 +14,45 @@ public class StoreConverter {
         return Store.builder()
                 .name(request.getName())
                 .address(request.getSpecAddress())
+=======
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.spring.domain.Region;
+import umc.spring.domain.Store;
+import umc.spring.repository.StoreRepository.StoreRepository;
+import umc.spring.service.RegionService.RegionService;
+import umc.spring.web.dto.StoreRequestDTO;
+import umc.spring.web.dto.StoreResponseDTO;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class StoreConverter {
+
+    private final RegionService regionService;
+
+    public Store toStore(StoreRequestDTO.createDto request){
+        Region region = regionService.regionById(request.getRegionId());
+
+        return Store.builder()
+                .name(request.getName())
+>>>>>>> finger9999
                 .region(region)
                 .build();
     }
 
+<<<<<<< HEAD
     public static StoreResponseDTO.AddResultDTO toAddResultDTO(Store store) {
         return StoreResponseDTO.AddResultDTO.builder()
                 .storeId(store.getId())
+=======
+    public static StoreResponseDTO.createResultDTO storeToStoreResponseDTO(Store store){
+        return StoreResponseDTO.createResultDTO.builder()
+                .id(store.getId())
+                .name(store.getName())
+                .regionName(store.getRegion().getName())
+>>>>>>> finger9999
                 .createdAt(LocalDateTime.now())
                 .build();
     }
--- finger9999/spring/week8/src/main/java/umc/spring/service/StoreService/StoreCommandServiceImpl.java
+++ finger9999/spring/week8/src/main/java/umc/spring/service/StoreService/StoreCommandServiceImpl.java
@@ -3,6 +3,7 @@ package umc.spring.service.StoreService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+<<<<<<< HEAD
 import umc.spring.apiPayload.code.status.ErrorStatus;
 import umc.spring.apiPayload.exception.handler.RegionHandler;
 import umc.spring.converter.StoreConverter;
@@ -29,5 +30,22 @@ public class StoreCommandServiceImpl implements StoreCommandService {
         Store newStore = StoreConverter.toStore(request, region);
 
         return storeRepository.save(newStore);
+=======
+import umc.spring.converter.StoreConverter;
+import umc.spring.domain.Store;
+import umc.spring.web.dto.StoreRequestDTO;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StoreCommandServiceImpl implements StoreCommandService {
+
+    private final StoreConverter storeConverter;
+
+    @Override
+    public Store createStore(StoreRequestDTO.createDto request) {
+        Store newStore = storeConverter.toStore(request);
+
+>>>>>>> finger9999
     }
 }
