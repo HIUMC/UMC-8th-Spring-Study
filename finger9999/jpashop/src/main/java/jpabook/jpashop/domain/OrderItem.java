826ad19b2bbcdbcef2ca16d2386a451bb41befdd
finger9999 <161580182+finger9999@users.noreply.github.com>
2025-05-19 13:08:15 +0900

    8주차 과제

--- finger9999/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ finger9999/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,4 +25,27 @@ public class OrderItem {
 
     private int count;
 
+    protected OrderItem() {} //이렇게 생성하기
+
+    //Order의 createOrder처럼..
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //비즈니스로직
+    public void cancel(){
+        getItem().addStock(count);
+    }
+
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+
 }
--- finger9999/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ finger9999/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -29,9 +29,14 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
-
+/*
     // 비즈니스 로직
     public void addQuantity(int quantity) {
+        this.stockQuantity += quantity;
+    }
+*/
+
+    public void addStock(int quantity) {
         this.stockQuantity += quantity;
     }
 
